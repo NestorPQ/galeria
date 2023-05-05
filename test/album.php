1f35ce0deabd833a46d96462e95dc4a771e1a979
Nestorpq <128406378+NestorPQ@users.noreply.github.com>
2023-05-04 23:27:24 -0500

    galeria con filtro y previsualización

---
+++ test/album.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>El cafe de Quique</title>
+
+</head>
+
+<body>
+
+  <?php
+  include 'header.html';
+  ?>
+
+  <main>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <h1 class="font-weight-light">Boda Jorge y Vania</h1>
+          <p class="lead text-muted">
+            Un amor a prueba de todo, que ni la circunstancias adversas pudo
+            romper con ese amor que hoy se profesan, y es que cuando el
+            Creador tiene cosas grandes para ti, no hay nada que se pueda
+            interponer. Y este es el caso de Jorge y Vania, que siendo
+            enamorados sufrieron un terrible accidente de tránsito en un bus
+            de servicio turístico en una tarde fría de invierno. La mas
+            afectada fue Vania, en donde su estado de su salud era reservado y
+            poco alentadores. Pero Dios tenía un plan para ella, contra todo
+            pronóstico ella se recuperó milagrosamente y sin secuelas. La vida
+            le dio una segunda oportunidad para seguir amando, para que ambos
+            se sigan amando, y dos años después de aquella tragedia, se den el
+            "si prometo amarte para toda la vida.
+          </p>
+          <!-- <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          </p> -->
+        </div>
+      </div>
+    </section>
+
+    <?php
+    include 'galeria-filtrable-marcos.html';
+    ?>
+
+  </main>
+
+  <footer class="text-muted py-5">
+    <div class="container">
+      <p class="float-right mb-1">
+        <a href="#">Regresar al principio</a>
+      </p>
+      <p class="mb-1">&copy; Album de ejemplo</p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
